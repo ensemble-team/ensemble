1564dfa36c34b4bc0b56c0a1e77d9fe5786a263d
Frankie Bell <fbell.pro@gmail.com>
2016-12-05 16:17:43 +0000

    test for form passes

--- spec/features/create_new_project_page_spec.rb
+++ spec/features/create_new_project_page_spec.rb
@@ -6,4 +6,13 @@ RSpec.feature 'User creates new project', :type => :feature do
 
     expect(page).to have_content 'New Project'
   end
+
+  scenario 'user can input project details' do
+    visit new_project_path
+
+    expect(page).to have_content 'Title'
+    expect(page).to have_content 'Genre'
+    expect(page).to have_content 'Description'
+    page.has_button? 'Create Project'
+  end
 end

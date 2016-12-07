09496ec62f30660034c23d7ca9414c0e241e96d7
Laurent Bouhnik <misterbl@hotmail.com>
2016-12-07 16:07:34 +0000

    facebook working a a few tests

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,24 @@ ActiveRecord::Schema.define(version: 20161207142130) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "authentications", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "token"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "commenter"
+    t.text     "body"
+    t.integer  "track_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["track_id"], name: "index_comments_on_track_id", using: :btree
+  end
+
   create_table "projects", force: :cascade do |t|
     t.string   "title"
     t.string   "genre"

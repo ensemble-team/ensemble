d69823661000cb23627ff665848a42734592fb03
Tom Page <tom.w.page@gmail.com>
2016-12-07 17:51:39 +0000

    nearly all tests passing

--- spec/features/upload_spec.rb
+++ spec/features/upload_spec.rb
@@ -5,15 +5,18 @@ RSpec.feature "Project", :type => :feature  do
 
   before(:each) do
     sign_in
+    @project = create(:project)
   end
 
   it "creates a new project" do
-    visit '/projects/new'
-    fill_in "project_title", with: "Nice Song"
-    fill_in "project_text", with: "Sexy"
-    
-    click_button "Upload Track"
+
+    visit "/projects/#{@project.id}"
+    fill_in "Title", with: "Nice Song"
+    fill_in "Text", with: "Sexy"
+    attach_file 'track_avatar', './spec/fixtures/files/mpthreetest.mp3'
+    click_button "Create Track"
     expect(page).to have_content("Track uploaded")
+    expect(page).to have_css("audio")
   end
 
 end

b411d9c1c3c07b86bc8e09900c6f13657392c371
Felix Harrison <fmlharrison@gmail.com>
2016-12-09 16:53:29 +0000

    all tests passing

--- spec/features/create_request_spec.rb
+++ spec/features/create_request_spec.rb
@@ -7,7 +7,7 @@ RSpec.feature 'User creates new request', :type => :feature do
     fill_in "Message", with: "Please accept"
     click_button "Create Request"
     expect(page).to have_content "Request sent successfully"
-
+  end
   # scenario 'user can send a request on a project page' do
   #   create_project
   #   fill_in "Message", with: "Please accept"

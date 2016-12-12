5d3a4095363b480c8c1f46a2bad846febc061298
Felix Harrison <fmlharrison@gmail.com>
2016-12-12 12:51:38 +0000

    added inbox to user dashboard

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,22 @@
 class UsersController < ApplicationController
 
+  helper_method :find_sender
+  helper :all
+  before_action :user_messages
+
   def show
   @user = User.find(params[:id])
   end
+
+  def find_sender(message)
+    User.find(message.sender)
+  end
+
+  private
+
+  def user_messages
+    @messages = Message.where(recipient: params[:id])
+  end
+
+
 end

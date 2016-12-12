fe12e8eb7014730468ae7336a66981e63c47e75a
Tom Page <tom.w.page@gmail.com>
2016-12-12 15:18:59 +0000

    test sees request button

--- spec/features/branch_merge_spec.rb
+++ spec/features/branch_merge_spec.rb
@@ -4,7 +4,7 @@ RSpec.feature 'User can request merge', :type => :feature, focus: true do
 
   scenario "create project and then create merge request" do
     create_branch_as_user_3
-    expect(page).to have_content("Create Request")
+    expect(page).to have_button("Create Request")
   end
 
 

81c8da5f9b0f04a564344fb67fb0aec1c86c3af4
Laurent Bouhnik <misterbl@hotmail.com>
2016-12-12 15:50:30 +0000

    refactored

--- db/schema.rb
+++ db/schema.rb
@@ -120,6 +120,8 @@ ActiveRecord::Schema.define(version: 20161212152255) do
     t.string   "uid"
     t.string   "image"
     t.string   "username"
+    t.string   "role"
+    t.integer  "role_id"
     t.text     "bio"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree

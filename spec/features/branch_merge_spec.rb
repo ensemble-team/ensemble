0990f0dfc899aa3b8116cbf3fd77436f25dc480a
Tom Page <tom.w.page@gmail.com>
2016-12-12 15:34:38 +0000

    can press request mix button test

--- spec/features/branch_merge_spec.rb
+++ spec/features/branch_merge_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.feature 'User can request merge', :type => :feature, focus: true do
+RSpec.feature 'User can request merge', :type => :feature do
 
   scenario "create project and then create merge request" do
     create_branch_as_user_3
@@ -8,10 +8,11 @@ RSpec.feature 'User can request merge', :type => :feature, focus: true do
   end
 
 
-  scenario "User should be able to click request merge button" do
+  scenario "User should be able to click request merge button", focus: true  do
     create_branch_as_user_3
-    expect{ click_link_or_button 'Create Request'}.to have_http_status(:redirect)
-    expect(flash[:notice]).to match(/^Request sent successfully/)
+    fill_in "Mix Request", with: "Please accept"
+    click_link_or_button('Create Request')
+    expect(page).to have_content("Request sent successfully")
   end
 
 end

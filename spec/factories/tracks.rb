5b9733aa49096f42c095987807019ede6644bec2
lcbeh <lichien2010@yahoo.co.uk>
2016-12-13 17:28:11 +0000

    add mix request test

--- spec/factories/tracks.rb
+++ spec/factories/tracks.rb
@@ -3,7 +3,7 @@ FactoryGirl.define do
     title "Lovely track"
     text "This is a lovely track"
     avatar Rack::Test::UploadedFile.new(File.open(File.join(Rails.root,"/spec/fixtures/files/mpthreetest.mp3")))
-    track_owner_id 1
-    track_owner_type ""
+    owner_id 1
+    owner_type ""
   end
 end

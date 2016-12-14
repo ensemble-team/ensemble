014411f059fdc600bfce5ec5eb008ef981c5dc92
Frankie Bell <fbell.pro@gmail.com>
2016-12-14 22:10:15 +0000

    profile drop down menu fixed - navbar message before_action causing problems

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!, except: [:index]
   before_action :navbar_message
-  skip_before_action :navbar_message, if: :devise_controller?
+  # skip_before_action :navbar_message, if: :devise_controller?
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
